b1468cdfe9467f394b75f9b88930d4c8d365f4bd
GULAM AHMED RAZA <54493079+glmoo7git@users.noreply.github.com>
2020-03-05 13:37:56 +0530

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,4 +1,7 @@
-QuickSort
-Like Merge Sort, QuickSort is a Divide and Conquer algorithm.
-It picks an element as pivot and partitions the given array around the picked pivot.
-There are many different versions of quickSort that pick pivot in different ways.
+# QuickSort
+# Like Merge Sort, QuickSort is a Divide and Conquer algorithm.
+# It picks an element as pivot and partitions the given array around the picked pivot.
+# There are many different versions of quickSort that pick pivot in different ways.
+
+# Always pick last element as pivot (implemented below)
+
